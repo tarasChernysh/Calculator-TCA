34a825f75f98f52148b6ceb6c9421a9d2cffd7ac
Taras C <taras.chernysh.ext@dkv-mobility.com>
2022-08-22 09:29:31 +0300

    init commit

---
+++ Calulator-TCA/Calulator-TCA/Calulator_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calulator_TCAApp.swift
+//  Calulator-TCA
+//
+//  Created by tchernysh on 22.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct Calulator_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
