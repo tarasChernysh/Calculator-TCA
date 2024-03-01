4b010d7a903dbfe620cc36d74cf0efb60a4ee74b
Taras C <taras.chernysh.ext@dkv-mobility.com>
2024-03-01 12:44:50 +0200

    add test

--- Calulator-TCA/Shared/SomeAnyDemo.swift
+++ Calulator-TCA/Shared/SomeAnyDemo.swift
@@ -8,45 +8,45 @@
 import Foundation
 
 protocol Student {
-    associatedtype ExamToPrepare: Exam
-    func prepare(for exam: ExamToPrepare)
+	associatedtype ExamToPrepare: Exam
+	func prepare(for exam: ExamToPrepare)
 }
 
 protocol Exam {
-    associatedtype Chapter: SubjectChapter where Chapter.Subject == Self
-    static func getChapter() -> Chapter
+	associatedtype Chapter: SubjectChapter where Chapter.Subject == Self
+	static func getChapter() -> Chapter
 }
 
 protocol SubjectChapter {
-    associatedtype Subject: Exam where Subject.Chapter == Self
-    func getExam() -> Subject
+	associatedtype Subject: Exam where Subject.Chapter == Self
+	func getExam() -> Subject
 }
 
 final class School {
-    
-    func preparing(student: some Student) {
-        let chapter = type(of: student).ExamToPrepare.getChapter()
-        let exam = chapter.getExam()
-        student.prepare(for: exam)
-    }
-    
-    func preparing2<S: Student>(student: S) {
-        let chapter = type(of: student).ExamToPrepare.getChapter()
-        let exam = chapter.getExam()
-        student.prepare(for: exam)
-    }
-    
-    func preparing2<S: Student>(students: [S]) {
-        
-        for student in students {
-            preparing(student: student)
-        }
-    }
-    
-    func preparing(students: [any Student]) {
-        let human = Human()
-        for student in students {
-            preparing(student: student)
-        }
-    }
+	func preparing(student: some Student) {
+		let chapter = type(of: student).ExamToPrepare.getChapter()
+		let exam = chapter.getExam()
+		student.prepare(for: exam)
+	}
+
+	func preparing2<S: Student>(student: S) {
+		let chapter = type(of: student).ExamToPrepare.getChapter()
+		let exam = chapter.getExam()
+		student.prepare(for: exam)
+	}
+
+	func preparing2<S: Student>(students: [S]) {
+		for student in students {
+			preparing(student: student)
+		}
+	}
+
+	func preparing(students: [any Student]) {
+		let human = Human()
+		for student in students {
+			preparing(student: student)
+		}
+	}
 }
+
+class University {}
