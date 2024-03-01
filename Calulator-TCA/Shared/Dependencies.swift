987a04f37b7f18989a9f98604874e772c63c0a6b
Taras C <taras.chernysh.ext@dkv-mobility.com>
2024-03-01 10:59:32 +0200

    update

--- Calulator-TCA/Shared/Dependencies.swift
+++ Calulator-TCA/Shared/Dependencies.swift
@@ -8,38 +8,48 @@
 import ComposableArchitecture
 import Foundation
 
-extension CalculatorService: DependencyKey {
-	static var liveValue: CalculatorService {
+extension CalculatorService: DependencyKey
+{
+	static var liveValue: CalculatorService
+	{
 		CalculatorService()
 	}
 }
 
-extension NumberConverterService: DependencyKey {
-	static var liveValue: NumberConverterService {
+extension NumberConverterService: DependencyKey
+{
+	static var liveValue: NumberConverterService
+	{
 		NumberConverterService()
 	}
 }
 
 // MARK: - Values
 
-extension DependencyValues {
-	var calculatorService: CalculatorService {
+extension DependencyValues
+{
+	var calculatorService: CalculatorService
+	{
 		get { self[CalculatorService.self] }
 		set { self[CalculatorService.self] = newValue }
 	}
 
-	var convertorService: NumberConverterService {
+	var convertorService: NumberConverterService
+	{
 		get { self[NumberConverterService.self] }
 		set { self[NumberConverterService.self] = newValue }
 	}
 }
 
-
-protocol Person {
-    func greeting(name: String)
+protocol Person
+{
+	func greeting(name: String)
 }
 
-struct Human: Person {
-    func greeting(name: String) {
-    }
+struct Human: Person
+{
+	func greeting(name _: String)
+	{
+		// hello
+	}
 }
