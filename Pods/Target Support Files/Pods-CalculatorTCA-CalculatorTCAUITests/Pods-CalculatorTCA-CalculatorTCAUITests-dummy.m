37ad3672c73a73c7578a35e9f505902770f44510
Taras C <taras.chernysh.ext@dkv-mobility.com>
2024-03-07 07:11:30 +0200

    update gitignore

--- Pods/Target Support Files/Pods-CalculatorTCA-CalculatorTCAUITests/Pods-CalculatorTCA-CalculatorTCAUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CalculatorTCA_CalculatorTCAUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CalculatorTCA_CalculatorTCAUITests
-@end
--- Pods/Target Support Files/Pods-CalculatorTCA/Pods-CalculatorTCA-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CalculatorTCA : NSObject
-@end
-@implementation PodsDummy_Pods_CalculatorTCA
-@end
