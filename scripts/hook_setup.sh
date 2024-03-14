4da48a9f0b4a6deddd6589f42ed6fe6f80f06336
Taras C <taras.chernysh.ext@dkv-mobility.com>
2024-03-14 22:08:35 +0200

    update moving git hook shell

--- scripts/hook_setup.sh
+++ scripts/hook_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-GIT_HOOKS_DIRECTORY=.git/hooks
-SOURCE_DIR=scripts/hooks
+ROOT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && cd ../ && pwd )"
+GIT_HOOKS_DIRECTORY=${ROOT_DIRECTORY}/.git/hooks
+SOURCE_DIR=${ROOT_DIRECTORY}/scripts/hooks
 HOOK_NAME=pre-commit
 
 if [ ! -d $GIT_HOOKS_DIRECTORY ]; then
@@ -9,9 +10,6 @@ if [ ! -d $GIT_HOOKS_DIRECTORY ]; then
     echo "hooks dir created"
 fi
 
-echo "git dir $(pwd)/${GIT_HOOKS_DIRECTORY}"
-echo "hook dir $(pwd)/$SOURCE_DIR/$HOOK_NAME"
-
-cp $(pwd)/$SOURCE_DIR/$HOOK_NAME $(pwd)/${GIT_HOOKS_DIRECTORY}/$HOOK_NAME
+cp $SOURCE_DIR/$HOOK_NAME ${GIT_HOOKS_DIRECTORY}/$HOOK_NAME
 chmod u+x ${GIT_HOOKS_DIRECTORY}/$HOOK_NAME
 echo "pre-commit hook added"
